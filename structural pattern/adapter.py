2a67ed017399cc8d8c0ada10554ef40bed44e9d2
Jieyi Wu <wu.jieyi@brilliantservice.co.jp>
2016-03-22 15:21:31 +0900

    1. Add Adapter pattern.

--- structural pattern/adapter.py
+++ structural pattern/adapter.py
@@ -1,8 +1,38 @@
 """ Created by wu.jieyi on 2016/03/08. """
+from abc import ABCMeta, abstractmethod
+
+
+class Rectangle(metaclass=ABCMeta):
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+# Adaptee.
+class LegacyRectangle:
+    def __init__(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+
+    def legacy_draw(self):
+        print('Legacy onDraw()')
+        print('x1:', self._x1, 'x2:', self._x2, 'y1:', self._y1, 'y2:', self._y2)
+
+
+# Adapter.
+class RectangleAdapter(Rectangle):
+    def __init__(self, x, y, w, h):
+        self._rectangle = LegacyRectangle(x, y, x + w, y + h)
+
+    def draw(self):
+        self._rectangle.legacy_draw()
 
 
 def main():
-    print("hello world")
+    rec = RectangleAdapter(10, 30, 15, 20)
+    rec.draw()
 
 
 if __name__ == '__main__':
